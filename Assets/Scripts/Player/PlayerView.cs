f5cc11d2891232208ba8a9d4619b13be49f6bd4c
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-09-01 17:20:32 +0900

    [add]フォルダの追加とプレイヤー用のScriptを作成

---
+++ Assets/Scripts/Player/PlayerView.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class PlayerView : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+    
+    #region coroutine method
+    #endregion
+}
