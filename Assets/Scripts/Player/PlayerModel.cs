640d26e72f1848046bc7b4f0b31473dcd76f50c6
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-09-04 18:09:42 +0900

    [update]PlayerのPrefabを修正、InputSystemの入力処理に変更

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UniRx;
+using UniRx.Triggers;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerModel : MonoBehaviour
 {
     #region property
     #endregion
 
     #region serialize
+    [SerializeField]
+    private float _moveSpeed = 5.0f;
+
+    [SerializeField]
+    private float rotateSpeed = 5.0f;
     #endregion
 
     #region private
+    private PlayerInput _input;
+    private Rigidbody _rb;
+
+    private Vector2 _inputAxis;
     #endregion
 
     #region Constant
@@ -23,12 +36,25 @@ public class PlayerModel : MonoBehaviour
     #region unity methods
     private void Awake()
     {
-
+        _input = GetComponent<PlayerInput>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Start()
     {
+        this.UpdateAsObservable()
+            .TakeUntilDestroy(this)
+            .Subscribe(_ =>
+            {
+                ApplyInput();
+            });
 
+        this.FixedUpdateAsObservable()
+            .TakeUntilDestroy(this)
+            .Subscribe(_ =>
+            {
+                ApplyMoving();
+            });
     }
     #endregion
 
@@ -36,8 +62,26 @@ public class PlayerModel : MonoBehaviour
     #endregion
 
     #region private method
+    private void ApplyInput()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        _inputAxis = new Vector2(h, v);
+    }
+
+    private void ApplyMoving()
+    {
+        var dir = new Vector3(_inputAxis.x, 0f, _inputAxis.y);
+        _rb.AddForce(dir.normalized * _moveSpeed, ForceMode.Force);
+    }
+
+    private void ApplyRotate()
+    {
+
+    }
     #endregion
-    
+
     #region coroutine method
     #endregion
 }
