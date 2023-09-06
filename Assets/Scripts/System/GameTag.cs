36eca4987f8313bdf234d2a1c2d75aff4571f99a
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-09-06 18:01:08 +0900

    [add]ゲーム中に使用するタグをまとめたクラスを追加

---
+++ Assets/Scripts/System/GameTag.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public static class GameTag
+{
+    #region property
+    public static string Player => _player;
+    public static string Obstacle => _obstacle;
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    private static string _player = "Player";
+    private static string _obstacle = "Obstacle";
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+    
+    #region coroutine method
+    #endregion
+}
