2f77370641493f291d5e74f45cbdfa3cae27af3d
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-09-07 17:59:43 +0900

    [add]ダメージを受けた際の処理に使用するInterfaceを作成

---
+++ Assets/Scripts/Interface/IDamagable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+/// <summary>
+/// ダメージを受ける機能のインターフェース
+/// </summary>
+public interface IDamagable
+{
+    #region property
+    /// <summary>無敵状態かどうか</summary>
+    bool IsInvincible { get; }
+    #endregion
+
+    #region public method
+    /// <summary>
+    /// ダメージを受ける
+    /// </summary>
+    /// <param name="amount">ダメージ量</param>
+    void Damage(float amount);
+    void Knockback(Transform other);
+    #endregion
+}
