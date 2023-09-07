39e1b6038cb18013ec9b796b97abc40ac69040a9
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-09-07 17:57:53 +0900

    [update]タグとレイヤーを新しく追加

--- Assets/Scripts/System/GameTag.cs
+++ Assets/Scripts/System/GameTag.cs
@@ -8,31 +8,12 @@ public static class GameTag
     #region property
     public static string Player => _player;
     public static string Obstacle => _obstacle;
-    #endregion
-
-    #region serialize
+    public static string Item => _item;
     #endregion
 
     #region private
     private static string _player = "Player";
     private static string _obstacle = "Obstacle";
-    #endregion
-
-    #region Constant
-    #endregion
-
-    #region Event
-    #endregion
-
-    #region unity methods
-    #endregion
-
-    #region public method
-    #endregion
-
-    #region private method
-    #endregion
-    
-    #region coroutine method
+    private static string _item = "Item";
     #endregion
 }
