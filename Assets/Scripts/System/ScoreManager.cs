d1354537830c74cb6e4ff1f282b08992eb13c166
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-09-28 10:13:18 +0900

    [update]スコア管理機能を修正

--- Assets/Scripts/System/ScoreManager.cs
+++ Assets/Scripts/System/ScoreManager.cs
@@ -8,14 +8,17 @@ public class ScoreManager : MonoBehaviour
 {
     #region property
     public static ScoreManager Instance { get; private set; }
-    public IObservable<int> CurrentScoreRP => _currentScore;
+    public IObservable<int> CurrentScoreObserver => _currentScoreRP;
+    public int CurrentScore => _currentScoreRP.Value;
     #endregion
 
     #region serialize
+    [SerializeField]
+    private ScoreView _scoreView = default;
     #endregion
 
     #region private
-    private ReactiveProperty<int> _currentScore = new ReactiveProperty<int>();
+    private ReactiveProperty<int> _currentScoreRP = new ReactiveProperty<int>();
     #endregion
 
     #region Constant
@@ -32,23 +35,37 @@ public class ScoreManager : MonoBehaviour
 
     private void Start()
     {
-        GameManager.Instance.AddScoreObserver
+        GameManager.Instance.UpdateScoreObserver
                             .TakeUntilDestroy(this)
                             .Subscribe(value => AddScore(value));
+
+        GameManager.Instance.GameResetObserver
+                            .TakeUntilDestroy(this)
+                            .Subscribe(_ => ResetScore());
     }
     #endregion
 
     #region public method
+    /// <summary>
+    /// スコアを加算する
+    /// </summary>
+    /// <param name="score">スコア</param>
     public void AddScore(int score)
     {
-        _currentScore.Value += score;
+        _currentScoreRP.Value += score;
+    }
+    public void ChangeScoreBoardView(bool value)
+    {
+
     }
     #endregion
-
+    /// <summary>
+    /// スコアをリセットする
+    /// </summary>
     #region private method
     private void ResetScore()
     {
-        _currentScore.Value = 0;
+        _currentScoreRP.Value = 0;
     }
     #endregion
     
